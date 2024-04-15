c07935a9e02be846a3af7113b695740bd436090c
Tassadar921 <paul.lecuisinier@gmail.com>
2024-04-15 16:21:00 +0200

    globals & social-media

---
+++ src/components/social-media/index.jsx
@@ -0,0 +1,8 @@
+import './index.scss';
+import { Icon } from "@iconify/react";
+
+export default function SocialMedia({ name }) {
+    return <div className="social-media">
+        <Icon icon={`brandico:${name}`} />
+    </div>;
+}
