5d85be4b7c1b8ef7d12d817ac38fbcff0084f564
MWJulien <j.fournier@mentalworks.fr>
2024-04-16 13:35:23 +0200

    feat companies forward

---
+++ src/components/CompaniesForward/index.jsx
@@ -0,0 +1,27 @@
+import './index.scss';
+import {Icon} from "@iconify/react";
+
+export default function CompaniesForward() {
+    return (
+        <div className="companies-forward">
+            <h2>Entreprise à la une</h2>
+            <ul className="companies-forward-img">
+                <li>
+                    <img src="https://via.placeholder.com/250x100" alt="placeholder"/>
+                </li>
+                <li>
+                    <img src="https://via.placeholder.com/250x100" alt="placeholder"/>
+                </li>
+                <li>
+                    <img src="https://via.placeholder.com/250x100" alt="placeholder"/>
+                </li>
+                <li>
+                    <img src="https://via.placeholder.com/250x100" alt="placeholder"/>
+                </li>
+                <li>
+                    <img src="https://via.placeholder.com/250x100" alt="placeholder"/>
+                </li>
+            </ul>
+        </div>
+    );
+}
