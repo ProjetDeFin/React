d5e894f17209074a9154736fc95e43cecd55396c
Tassadar921 <paul.lecuisinier@gmail.com>
2024-04-16 15:04:14 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Error from "./pages/Error";
 import Home from "./pages/Home";
@@ -10,6 +11,57 @@ import Students from "./pages/Students";
 import Register from "./pages/Register";
 import Layout from "./components/Layout";
 
+=======
+import Error from './pages/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Applications from './pages/Applications';
+import Companies from './pages/Companies';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import Students from './pages/Students';
+import Register from './pages/Register';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/applications',
+    element: <Applications />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/companies',
+    element: <Companies />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/offers',
+    element: <Offers />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/students',
+    element: <Students />,
+    errorElement: <Error />,
+  },
+]);
+
+>>>>>>> ba460a5 (prettier)
 export default function App() {
   return (
     <Router>
