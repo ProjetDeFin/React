77ea13abbd5ead793dbda1f6d5622be3669883aa
Tassadar921 <paul.lecuisinier@gmail.com>
2024-04-16 15:04:14 +0200

    prettier

--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NavLink } from "react-router-dom";
 import "./index.scss";
 
@@ -35,5 +36,50 @@ export default function Navbar() {
         </div>
         </nav>
     </div>
+=======
+import Link from '../../Link';
+import Item from './Item';
+import './index.scss';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/" children="Logo" />
+      <ul className="navbar-list">
+        <li>
+          <Item href="/" children="Accueil" />
+        </li>
+        <li>
+          <Item href="/offers" children="Offres" />
+        </li>
+        <li>
+          <Item href="/applications" children="Demandes" />
+        </li>
+        <li>
+          <Item href="/companies" children="Entreprise" />
+        </li>
+        <li>
+          <Item href="/students" children="Etudiants" />
+        </li>
+      </ul>
+      <ul className="account">
+        <li>
+          <Item
+            href="/sign-in"
+            children="Se connecter"
+            classes="sign-in btn-sign"
+          />
+        </li>
+        <li className="separator"></li>
+        <li>
+          <Item
+            href="/register"
+            children="Créer un compte"
+            classes="register btn-sign"
+          />
+        </li>
+      </ul>
+    </nav>
+>>>>>>> ba460a5 (prettier)
   );
 }
