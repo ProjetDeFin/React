32b588fcd5961e0bc879f75ab6f0c32525ac38aa
qrulleau <quentin.rulleau@yahoo.com>
2024-04-16 14:37:06 +0200

    MIN: refacto navbar by using navLink

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
 import './App.css';
-import Error from './pages/Error';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Applications from './pages/Applications';
-import Companies from './pages/Companies';
-import Offers from './pages/Offers';
-import SignIn from './pages/SignIn';
-import Students from './pages/Students';
-import Register from './pages/Register';
-import Layout from './components/Layout';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/applications',
-    element: <Applications />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/companies',
-    element: <Companies />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/offers',
-    element: <Offers />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/register',
-    element: <Register />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/students',
-    element: <Students />,
-    errorElement: <Error />,
-  },
-]);
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Applications from "./pages/Applications";
+import Companies from "./pages/Companies";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import Students from "./pages/Students";
+import Register from "./pages/Register";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
-    <div className="App">
-      <Layout children={<RouterProvider router={router} />} />
-    </div>
+    <Router>
+      <div className="App">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/applications" element={<Applications />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Layout>
+      </div>
+    </Router>
   );
 }
--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,45 +1,21 @@
-import Link from '../../Link';
-import Item from './Item';
+import { NavLink } from 'react-router-dom';
 import './index.scss';
 
 export default function Navbar() {
-  return (
-    <nav>
-      <Link href="/" children="Logo" />
-      <ul className="navbar-list">
-        <li>
-          <Item href="/" children="Accueil" />
-        </li>
-        <li>
-          <Item href="/offers" children="Offres" />
-        </li>
-        <li>
-          <Item href="/applications" children="Demandes" />
-        </li>
-        <li>
-          <Item href="/companies" children="Entreprise" />
-        </li>
-        <li>
-          <Item href="/students" children="Etudiants" />
-        </li>
-      </ul>
-      <ul className="account">
-        <li>
-          <Item
-            href="/sign-in"
-            children="Se connecter"
-            classes="sign-in btn-sign"
-          />
-        </li>
-        <li className="separator"></li>
-        <li>
-          <Item
-            href="/register"
-            children="Créer un compte"
-            classes="register btn-sign"
-          />
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="d-flex align-center">
+            <NavLink exact to="/" className="logo-link" activeClassName="is-active">Logo</NavLink>
+            <div className="d-flex">
+                <NavLink exact to="/" activeClassName="is-active">Accueil</NavLink>
+                <NavLink to="/offers" activeClassName="is-active">Offres</NavLink>
+                <NavLink to="/applications" activeClassName="is-active">Demandes</NavLink>
+                <NavLink to="/companies" activeClassName="is-active">Entreprise</NavLink>
+                <NavLink to="/students" activeClassName="is-active">Etudiants</NavLink>
+            </div>
+            <div className="d-flex">
+                <NavLink to="/sign-in" activeClassName="is-active">Se connecter</NavLink>
+                <NavLink to="/register" activeClassName="is-active">Creer un compte</NavLink>
+            </div>
+        </nav>
+    );
 }
