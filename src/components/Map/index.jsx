0e801e9714549dd1e00003b6d8aac8a3901cc216
MWJulien <j.fournier@mentalworks.fr>
2024-05-17 12:01:59 +0200

    feat google maps api component #19

---
+++ src/components/Map/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '600px',
+  height: '450px'
+};
+
+const Map = ({item}) => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+  const center = {
+    lat: item.lat,
+    lng: item.lng
+  };
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker position={ center } />
+    </GoogleMap>
+  ) : <></>
+};
+
+export default React.memo(Map);
