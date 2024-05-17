53073b50190bd5c556155c1c68f0375619d3fff3
Tassadar921 <paul.lecuisinier@gmail.com>
2024-05-17 14:10:24 +0200

    lint

--- src/components/CompaniesForward/index.jsx
+++ src/components/CompaniesForward/index.jsx
@@ -2,15 +2,15 @@ import './index.scss';
 
 export default function CompaniesForward({ companies }) {
   return (
-      <div className="companies-forward">
-        <h2>Entreprise à la une</h2>
-        <ul className="companies-forward-img">
-          {companies.slice(0,5).map((company) => (
-              <li key={company.name}>
-                <img src={company.logo} alt={company.name} />
-              </li>
-          ))}
-        </ul>
-      </div>
+    <div className="companies-forward">
+      <h2>Entreprise à la une</h2>
+      <ul className="companies-forward-img">
+        {companies.slice(0, 5).map((company) => (
+          <li key={company.name}>
+            <img src={company.logo} alt={company.name} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
--- src/components/Layout/Navbar/index.jsx
+++ src/components/Layout/Navbar/index.jsx
@@ -1,39 +1,43 @@
-import { NavLink } from "react-router-dom";
-import "./index.scss";
+import { NavLink } from 'react-router-dom';
+import './index.scss';
 
 export default function Navbar() {
   return (
     <div className="container">
-        <nav className="d-flex align-center">
+      <nav className="d-flex align-center">
         <NavLink exact to="/" className="logo-link" activeClassName="is-active">
-            Logo
+          Logo
         </NavLink>
         <div className="d-flex menu">
-            <NavLink exact to="/" activeClassName="is-active">
+          <NavLink exact to="/" activeClassName="is-active">
             Accueil
-            </NavLink>
-            <NavLink to="/offers" activeClassName="is-active">
+          </NavLink>
+          <NavLink to="/offers" activeClassName="is-active">
             Offres
-            </NavLink>
-            <NavLink to="/applications" activeClassName="is-active">
+          </NavLink>
+          <NavLink to="/applications" activeClassName="is-active">
             Demandes
-            </NavLink>
-            <NavLink to="/companies" activeClassName="is-active">
+          </NavLink>
+          <NavLink to="/companies" activeClassName="is-active">
             Entreprise
-            </NavLink>
-            <NavLink to="/students" activeClassName="is-active">
+          </NavLink>
+          <NavLink to="/students" activeClassName="is-active">
             Etudiants
-            </NavLink>
+          </NavLink>
         </div>
         <div className="d-flex account">
-            <NavLink to="/sign-in" activeClassName="is-active" className="btn btn-empty">
+          <NavLink
+            to="/sign-in"
+            activeClassName="is-active"
+            className="btn btn-empty"
+          >
             Se connecter
-            </NavLink>
-            <NavLink to="/register" activeClassName="is-active" className="btn">
+          </NavLink>
+          <NavLink to="/register" activeClassName="is-active" className="btn">
             Creer un compte
-            </NavLink>
+          </NavLink>
         </div>
-        </nav>
+      </nav>
     </div>
   );
 }
