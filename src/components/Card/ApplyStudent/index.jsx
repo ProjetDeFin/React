37a4f810ac8ea7047eae495cb714bda28da80273
qrulleau <quentin.rulleau@yahoo.com>
2024-06-01 01:22:01 +0200

    ADD: apply student section

---
+++ src/components/Card/ApplyStudent/index.jsx
@@ -0,0 +1,30 @@
+import './index.scss';
+
+export default function ApplyStudent({
+    profilPicture,
+    titleApply,
+    namePerson,
+    agePerson,
+    locationPerson,
+    tag,
+    periodApply
+}) {
+    return (
+        <div className="apply-student-card">
+            <div className="d-flex justify-start">
+                <div>
+                    <img src={`/img/profil-picture/${profilPicture}`} alt="" />
+                </div>
+                <div className="description">
+                    <h3>{titleApply}</h3>
+                    <p className="detail-apply">{namePerson} ({agePerson}) . {locationPerson}</p>
+                    <div className="d-flex">
+                        <p className="type-offer">{tag}</p>
+                        <span></span>
+                        <p className={periodApply}>{periodApply}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
