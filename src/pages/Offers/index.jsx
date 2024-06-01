c4435e0dd70320c7a75109ece7b083ca7fa23ea9
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2024-06-01 14:12:16 +0200

    Update index.jsx

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,15 +1,7 @@
-import Quill from '../../components/Form/Quill';
-import { useEffect, useState } from 'react';
-
 export default function Offers() {
-    const [value, setValue] = useState('');
-    useEffect(() => {
-        console.log(value);
-    }, [value]);
   return (
     <div>
       <h1>Offers</h1>
-        <Quill value={value} onChange={setValue}/>
     </div>
   );
 }
