2536c4cdcde2c814b8cda4ef6109a63b2648a69a
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2024-06-01 14:10:27 +0200

    quill & hooks lib

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,7 +1,15 @@
+import Quill from '../../components/Form/Quill';
+import { useEffect, useState } from 'react';
+
 export default function Offers() {
+    const [value, setValue] = useState('');
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
   return (
     <div>
       <h1>Offers</h1>
+        <Quill value={value} onChange={setValue}/>
     </div>
   );
 }
