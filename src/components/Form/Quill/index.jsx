bb23b29a09c79bd7f8af2642d53c0a020c76823f
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2024-06-01 14:10:27 +0200

    quill & hooks lib

---
+++ src/components/Form/Quill/index.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useCallback } from 'react';
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css';
+
+export default function Quill({ width = 500, height = 300, value, onChange }) {
+    const { quill, quillRef } = useQuill();
+    const debounceTimeout = useRef(null);
+
+    const handleTextChange = useCallback(() => {
+        if (debounceTimeout.current) {
+            clearTimeout(debounceTimeout.current);
+        }
+        debounceTimeout.current = setTimeout(() => {
+            if (quill) {
+                onChange(quill.root.innerHTML);
+            }
+        }, 500);
+    }, [onChange, quill]);
+
+    useEffect(() => {
+        if (quill) {
+            quill.on('text-change', handleTextChange);
+        }
+
+        return () => {
+            if (quill) {
+                quill.off('text-change', handleTextChange);
+            }
+            if (debounceTimeout.current) {
+                clearTimeout(debounceTimeout.current);
+            }
+        };
+    }, [quill, handleTextChange]);
+
+    return (
+        <div style={{ width, height }}>
+            <div ref={quillRef} />
+        </div>
+    );
+}
