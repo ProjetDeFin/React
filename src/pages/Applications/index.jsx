27fe48612a514c518c4bd1d7f025fd5bc76f03e3
MWJulien <j.fournier@mentalworks.fr>
2024-06-02 22:45:17 +0200

    fix filter offer

--- src/pages/Applications/index.jsx
+++ src/pages/Applications/index.jsx
@@ -2,7 +2,6 @@ export default function Applications() {
   return (
     <div className="Applications">
       <header className="Applications-header">
-        <p>Applications</p>
       </header>
     </div>
   );
