faab119ad48894ae625c74c081ea6431c0067772
qrulleau <quentin.rulleau@yahoo.com>
2024-06-15 21:57:18 +0200

    MIN: linter

--- src/pages/Authentification/Login/index.jsx
+++ src/pages/Authentification/Login/index.jsx
@@ -2,29 +2,41 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 
 export default function Login() {
-    return (
-        <div className="grey login">
-            <div className="d-flex justify-center align-center">
-                <div className="content">
-                    <div className="text-center">
-                        <h2>BIENVENUE</h2>
-                        <h3>Connectez vous pour continuer</h3>
-                        <p>pas encore de compte ? <Link>Cree en un !</Link></p>
-                        <form method="post">
-                            <div className="d-flex direction-column align-start">
-                                <label htmlFor="email">Email</label>
-                                <input type="text" name="email" id="email" placeholder="jean.bernard@gmail.com"/>
-                            </div>
-                            <div className="d-flex direction-column align-start">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" name="password" id="password" placeholder="G1BonMotdePasse!"/>
-                            </div>
-                            <input className="btn" type="submit" value="SE CONNECTER" />
-                            <p className="text-right">mot de passe oublie ?</p>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="grey login">
+      <div className="d-flex justify-center align-center">
+        <div className="content">
+          <div className="text-center">
+            <h2>BIENVENUE</h2>
+            <h3>Connectez vous pour continuer</h3>
+            <p>
+              pas encore de compte ? <Link>Cree en un !</Link>
+            </p>
+            <form method="post">
+              <div className="d-flex direction-column align-start">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="jean.bernard@gmail.com"
+                />
+              </div>
+              <div className="d-flex direction-column align-start">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="G1BonMotdePasse!"
+                />
+              </div>
+              <input className="btn" type="submit" value="SE CONNECTER" />
+              <p className="text-right">mot de passe oublie ?</p>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
