6a7f9f9d3799890c27848ce43675e26c994eb46b
qrulleau <quentin.rulleau@yahoo.com>
2024-06-15 22:40:01 +0200

    MIN: forget to add form tag

--- src/pages/Authentification/Registration/index.jsx
+++ src/pages/Authentification/Registration/index.jsx
@@ -7,23 +7,25 @@ export default function Registration() {
             <div className="content">
                 <h2>INSCRIPTION</h2>
                 <p>Rejoignez Bourses aux Stages et tentez votre chance parmis les offres déja en ligne !</p>
-                <div className="d-flex direction-column align-start">
-                  <label htmlFor="firstName">Prénom</label>
-                  <input type="text" name="firstName" id="firstName" placeholder="jean" />
-                </div>
-                <div className="d-flex direction-column align-start">
-                  <label htmlFor="name">Nom</label>
-                  <input type="text" name="name" id="name" placeholder="jean" />
-                </div>
-                <div className="d-flex direction-column align-start">
-                  <label htmlFor="email">E-mail</label>
-                  <input type="text" name="email" id="email" placeholder="jean" />
-                </div>
-                <div className="d-flex direction-column align-start">
-                  <label htmlFor="password">Mot de passe</label>
-                  <input type="text" name="password" id="password" placeholder="jean"/>
-                </div>
-                <input className="btn" type="submit" value="Créer mon compte" />
+                <form action="" method="post">
+                  <div className="d-flex direction-column align-start">
+                    <label htmlFor="firstName">Prénom</label>
+                    <input type="text" name="firstName" id="firstName" placeholder="jean" />
+                  </div>
+                  <div className="d-flex direction-column align-start">
+                    <label htmlFor="name">Nom</label>
+                    <input type="text" name="name" id="name" placeholder="jean" />
+                  </div>
+                  <div className="d-flex direction-column align-start">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="text" name="email" id="email" placeholder="jean" />
+                  </div>
+                  <div className="d-flex direction-column align-start">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="text" name="password" id="password" placeholder="jean"/>
+                  </div>
+                  <input className="btn" type="submit" value="Créer mon compte" />
+                </form>
             </div>
         </div>
     </div>
