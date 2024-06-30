e6a567d65e72ff0592e0e143b616ca01470e7984
qrulleau <quentin.rulleau@yahoo.com>
2024-06-30 16:27:58 +0200

    ADD: section my profil

---
+++ src/pages/Admin/Profil/index.jsx
@@ -0,0 +1,9 @@
+import FormRegistration from "../../../components/Form/Registration"
+
+export default function AdminProfil () {
+    return (
+        <div className="admin-profil">
+            <FormRegistration />
+        </div>
+    )
+}
