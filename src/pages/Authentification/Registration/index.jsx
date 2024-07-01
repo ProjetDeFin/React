5668d6bede231c2100d31314ea927c6d4cc7e4e1
MWJulien <j.fournier@mentalworks.fr>
2024-07-01 15:20:21 +0200

    fix import useState

--- src/pages/Authentification/Registration/index.jsx
+++ src/pages/Authentification/Registration/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { useState } from 'react';
 
 export default function Registration() {
   const [firstName, setFirstName] = useState('');
