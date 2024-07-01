27c593cf5a73d033304cfb77ba15ed819284d74e
MWJulien <j.fournier@mentalworks.fr>
2024-07-02 01:25:57 +0200

    fix link admin offer list

--- src/pages/Admin/Offer/index.jsx
+++ src/pages/Admin/Offer/index.jsx
@@ -2,72 +2,23 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import ListAdmin from '../../../components/List/Admin';
 import './index.scss';
+import { useEffect, useState } from 'react';
 
 export default function OfferAdmin() {
   const navigate = useNavigate();
+  const [offers, setOffers] = useState([]);
 
   if (localStorage.getItem('role') !== 'ROLE_COMPANY_RESPONSIBLE') {
     navigate('/admin/mon-profil');
   }
 
-  const offers = [
-    {
-      name: 'Développeur Front-End',
-      status: 'Active',
-      datePublication: '01/06/2024',
-      dateLimite: '30/06/2024',
-      type: 'Stage',
-      candidate: 12,
-    },
-    {
-      name: 'Assistant Marketing',
-      status: 'Cloture',
-      datePublication: '15/05/2024',
-      dateLimite: '15/06/2024',
-      type: 'Alternance',
-      candidate: 20,
-    },
-    {
-      name: 'Data Analyst',
-      status: 'Active',
-      datePublication: '10/06/2024',
-      dateLimite: '10/07/2024',
-      type: 'Stage',
-      candidate: 8,
-    },
-    {
-      name: 'Chef de Projet',
-      status: 'Cloture',
-      datePublication: '05/05/2024',
-      dateLimite: '05/06/2024',
-      type: 'Alternance',
-      candidate: 15,
-    },
-    {
-      name: 'Designer UX/UI',
-      status: 'Active',
-      datePublication: '20/06/2024',
-      dateLimite: '20/07/2024',
-      type: 'Stage',
-      candidate: 10,
-    },
-    {
-      name: 'Ingénieur Logiciel',
-      status: 'Active',
-      datePublication: '25/06/2024',
-      dateLimite: '25/07/2024',
-      type: 'Alternance',
-      candidate: 5,
-    },
-    {
-      name: 'Consultant IT',
-      status: 'Cloture',
-      datePublication: '01/04/2024',
-      dateLimite: '01/05/2024',
-      type: 'Stage',
-      candidate: 18,
-    },
-  ];
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/api/offers/company/${localStorage.getItem('id')}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOffers(data);
+      });
+  }, []);
 
   return (
     <div className="offer-admin">
@@ -85,8 +36,9 @@ export default function OfferAdmin() {
         <span></span>
         <section>
           <div className="d-flex">
-            <h3>16 offres trouvées</h3>
-            <div className="d-flex">
+            <h3>{offers.length} offres trouvées</h3>
+            {/*todo sort*/}
+            {/*<div className="d-flex">
               <p>Filter par status :</p>
               <select name="" id="">
                 <option value="Tous" default>
@@ -95,7 +47,7 @@ export default function OfferAdmin() {
                 <option value="Active">Active</option>
                 <option value="Clôturé">Clôturé</option>
               </select>
-            </div>
+            </div>*/}
           </div>
           <div className="table">
             <div className="head-table">
@@ -120,8 +72,17 @@ export default function OfferAdmin() {
                 </div>
               </div>
             </div>
-            <ListAdmin data={offers} />
-            <div className="row d-flex">{/* ajout de la pagination */}</div>
+            {offers.map((offer, index) => (
+              <div key={index} className="body-table">
+                <div className="name">{offer.title}</div>
+                <div className="status">{offer.deletedAt ? 'Active' : 'Clôturé'}</div>
+                <div className="date-publication">{offer.formatedStartApplyDate}</div>
+                <div className="date-limite">{offer.formatedEndApplyDate}</div>
+                <div className="type">{offer.type}</div>
+                <div className="candidate">{offer.applications.length}</div>
+              </div>
+            ))}
+            <div className="row d-flex">{/* todo ajout de la pagination */}</div>
           </div>
         </section>
       </div>
